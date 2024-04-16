0ffc235645247740863aa5336661cc60652544e2
lutaok <lucadig.1994@gmail.com>
2024-04-16 21:43:29 +0200

    feat: add github repo link for portfolio

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,8 +1,19 @@
 import { Component } from "solid-js";
+import Icon from "../Icon/Icon";
 
 const Footer: Component = () => {
   return (
-    <footer class="flex w-full justify-end rounded-t-2xl border-t-2 border-primary-500 bg-bgColor-950 px-16 py-4">
+    <footer class="flex w-full items-center justify-end gap-4 rounded-t-2xl border-t-2 border-primary-500 bg-bgColor-950 px-16 py-4">
+      <div>
+        <a
+          href={"https://github.com/lutaok/portfolio"}
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label={`Go to portfolio github repository`}
+        >
+          <Icon name={"github-logo"} />
+        </a>
+      </div>
       <div>
         Built with{" "}
         <a
